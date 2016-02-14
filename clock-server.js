9b0a506665b74017f3b5ad7425cd825502c8a05b
leosunsheng <su371128017@qq.com>
2016-02-14 17:29:57 +0800

    add

---
+++ clock-server.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Jack on 2016/2/14.
+ */
+var app = require('http').createServer(handler);
+var io = require('socket.io').listen(app);
+var fs = require('fs');
+var html = fs.readFileSync('index.html','utf8');
+
+function handler (req, res) {
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Content-Length', Buffer.byteLength(html, 'utf8'));
+    res.end(html);
+}
+
+function tick () {
+    var now = new Date().toUTCString();
+    io.socket.send(now);
+}
+
+setInterval(tick, 1000);
+
+app.listen(8080);
